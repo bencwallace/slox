ee60931c1470256e138d0c6d4e544c4c3df21e62
Ben <bencwallace@gmail.com>
2020-01-06 10:19:29 +0100

    refactored

--- src/main/scala/com/craftinginterpreters/lox/Value.scala
+++ src/main/scala/com/craftinginterpreters/lox/Value.scala
@@ -2,30 +2,30 @@ package com.craftinginterpreters.lox
 
 // double, boolean, nil, string
 sealed trait Value {
-  def isTruthy: Boolean = true
+  def isTruthy: Boolean = this match {
+    case Bool(value) => value
+    case NilVal => false
+    case _ => true
+  }
+
+  override def toString: String = this match {
+    case LoxCallable => "LoxCallable"
+    case Bool(value) => value.toString
+    case NilVal => "nil"
+    case Number(value) =>
+      val text = value.toString
+      if (text.endsWith(".0")) text.substring(0, text.length - 2)
+      else text
+    case Str(value) => value
+  }
 }
 
 abstract case class LoxCallable() extends Value {
-  override def toString: String = "LoxCallable"
   def arity: Int
   def call(interpreter: Interpreter, args: Seq[Value]): Value
 }
 
-case class Bool(value: Boolean) extends Value {
-  override def toString: String = value.toString
-  override def isTruthy: Boolean = value
-}
-case object NilVal extends Value {
-  override def toString: String = "nil"
-  override def isTruthy: Boolean = false
-}
-case class Number(value: Double) extends Value {
-  override def toString: String = {
-    val text = value.toString
-    if (text.endsWith(".0")) text.substring(0, text.length - 2)
-    else text
-  }
-}
-case class Str(value: String) extends Value {
-  override def toString: String = value
-}
+case class Bool(value: Boolean) extends Value
+case object NilVal extends Value
+case class Number(value: Double) extends Value
+case class Str(value: String) extends Value
